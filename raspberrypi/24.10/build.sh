e6fb8b6465e99784216d97de1b7141d3c4085512
an1234 <55307207+anandyuan@users.noreply.github.com>
2025-06-02 18:22:28 +0800

    update:   raspberrypi/24.10/komd.conf

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -17,10 +17,47 @@ PACKAGES="$PACKAGES curl wget"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 
 # USB转RJ45网卡支持 (AX88179B)
-PACKAGES="$PACKAGES kmod-usb-net kmod-usb-net-asix-ax88179"
+PACKAGES="$PACKAGES kmod-usb-net-asix-ax88179"
 PACKAGES="$PACKAGES kmod-usb-core kmod-usb-ohci kmod-usb-uhci kmod-usb2 kmod-usb3"
 PACKAGES="$PACKAGES kmod-usb-net-cdc-ether kmod-usb-net-rndis"
 PACKAGES="$PACKAGES kmod-tun"
+
+# 硬件支持
+# 无线网卡和USB网卡驱动支持
+PACKAGES="$PACKAGES kmod-rtl8187"
+PACKAGES="$PACKAGES kmod-rtl8188eu"
+PACKAGES="$PACKAGES kmod-rtl8192c-common"
+PACKAGES="$PACKAGES kmod-rtl8192cu"
+PACKAGES="$PACKAGES kmod-rtl8192du"
+PACKAGES="$PACKAGES kmod-rtl8812au-ac"
+PACKAGES="$PACKAGES kmod-rtl8821cu"
+PACKAGES="$PACKAGES kmod-rtl88x2bu"
+PACKAGES="$PACKAGES kmod-rtlwifi"
+PACKAGES="$PACKAGES kmod-rtlwifi-usb"
+PACKAGES="$PACKAGES kmod-usb-net-rtl8150"
+PACKAGES="$PACKAGES kmod-usb-net-rtl8152"
+
+# 增加更多 USB 网卡和网络相关驱动支持
+PACKAGES="$PACKAGES kmod-inet-diag"
+PACKAGES="$PACKAGES kmod-usb-net-asix"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-eem"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-mbim"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-ncm"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-subset"
+PACKAGES="$PACKAGES kmod-usb-net-dm9601-ether"
+PACKAGES="$PACKAGES kmod-usb-net-hso"
+PACKAGES="$PACKAGES kmod-usb-net-huawei-cdc-ncm"
+PACKAGES="$PACKAGES kmod-usb-net-ipheth"
+PACKAGES="$PACKAGES kmod-usb-net-kalmia"
+PACKAGES="$PACKAGES kmod-usb-net-kaweth"
+PACKAGES="$PACKAGES kmod-usb-net-mcs7830"
+PACKAGES="$PACKAGES kmod-usb-net-pegasus"
+PACKAGES="$PACKAGES kmod-usb-net-pl"
+PACKAGES="$PACKAGES kmod-usb-net-qmi-wwan"
+PACKAGES="$PACKAGES kmod-usb-net-sierrawireless"
+PACKAGES="$PACKAGES kmod-usb-net-smsc95xx"
+PACKAGES="$PACKAGES kmod-usb-net-sr9700"
+PACKAGES="$PACKAGES kmod-usb-net2280"
 
 # USB工具和调试工具
 PACKAGES="$PACKAGES usbutils"
@@ -65,6 +102,14 @@ PACKAGES="$PACKAGES python3 python3-pip"
 PACKAGES="$PACKAGES luci-app-wol"
 PACKAGES="$PACKAGES luci-app-alist luci-i18n-alist-zh-cn"
 PACKAGES="$PACKAGES luci-app-netdata"
+
+
+
+# 新增
+PACKAGES="$PACKAGES luci-app-eqos"
+PACKAGES="$PACKAGES luci-app-gowebdav"
+PACKAGES="$PACKAGES luci-app-openvpn-server"
+PACKAGES="$PACKAGES luci-app-samba"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
